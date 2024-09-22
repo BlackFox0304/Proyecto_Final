f4aa65410c850427cd5cdfdcb05b3055ceccb970
Citlali Hermosillo <mariacitlalihermosillo2004@gmail.com>
2024-09-22 17:08:54 -0600

    login (aun no es funcional)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,28 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Login</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <main class="container aling-center p-5">
-        <form method="post" action="{{route('validar-registro')}}">
+        <form method="post" action="{{route('inicia-sesion')}}">
             @csrf
             <div class="mb-3">
                 <label for="emailInput" class="form-label">Email</label>
-                <input type="email" class="form-control" id="emailInput" name="email" required autocomplete="disable">
+                <input type="email" class="form-control" id="emailInput" name="email" required>
             </div>
             <div class="mb-3">
                 <label for="passwordInput" class="form-label">Password</label>
                 <input type="password" class="form-control" id="passwordInput" name="password" required>
             </div>
-            <div class="mb-3">
-                <label for="userInput" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="userInput" name="name" required autocomplete="disable">
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="rememberCheck" name="remember">
+                <label class="form-check-label" for="rememberCheck">Mantener sesión iniciada</label>
             </div>
-            <button type="submit" class="btn btn-primary">Registrarse</button>
+            <div>
+                <p>¿No tienes cuenta? Registrate <a href="{{route('registro')}}">aquí</a></p>
+            </div>
+            <button type="submit" class="btn btn-primary">Acceder</button>
         </form>
     </main>
 </body>
