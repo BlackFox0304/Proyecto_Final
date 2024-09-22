ec56b4e7f73a1dec3ce89e3683cd68781b3c894e
Citlali Hermosillo <mariacitlalihermosillo2004@gmail.com>
2024-09-22 16:55:21 -0600

    registro (aun no es funcional)

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container aling-center p=5">
+        <form method="post" action="{{route('validar-registro')}}">
+            @csrf
+            <div class="mb=3">
+                <label for="emailInput" class="form-label">Email</label>
+                <input type="email" class="form-control" id="emailInput" name="email" required autocomplete="disable">
+            </div>
+            <div class="mb-3">
+                <label for="passwordInput" class="form-label">Password</label>
+                <input type="passwordInput" class="form-control" id="passwordInput" name="password" required>
+            </div>
+            <div class="mb-3">
+                <label for="userInput" class="form-label">Nombre</label>
+                <input type="usarInput" class="form-control" id="userInput" name="name" required autocomplete="disable">
+            </div>
+            <button type="submit" class="btn btn-primary">Registrarse</button>
+        </form>
+    </main>
+</body>
+
+</html>
